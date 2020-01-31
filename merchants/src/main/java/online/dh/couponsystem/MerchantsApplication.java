fa59181ed06e08b580f876b990ccbffe4f023fe5
D_h <54danghao@gmail.com>
2020-01-31 17:25:50 +0800

    自定义拦截器

--- merchants/src/main/java/online/dh/couponsystem/MerchantsApplication.java
+++ merchants/src/main/java/online/dh/couponsystem/MerchantsApplication.java
@@ -1,14 +1,30 @@
 package online.dh.couponsystem;
 
+import online.dh.couponsystem.security.AuthCheckInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class MerchantsApplication {
+public class MerchantsApplication extends WebMvcConfigurerAdapter {
+
+	// 注入自定义拦截器
+	@Resource
+	private AuthCheckInterceptor authCheckInterceptor;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(MerchantsApplication.class, args);
 	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		// 先注册拦截器，再配置拦截器拦截哪些请求
+		registry.addInterceptor(authCheckInterceptor).addPathPatterns("/merchants/**");
+		super.addInterceptors(registry);
+	}
 }
