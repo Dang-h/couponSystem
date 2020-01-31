9787b9369dcfc754443db32a3391f2da97b164a6
D_h <54danghao@gmail.com>
2020-01-31 14:17:07 +0800

    优惠券投放接口实现测试

--- merchants/src/test/java/online/dh/couponsystem/service/MerchantsServeTest.java
+++ merchants/src/test/java/online/dh/couponsystem/service/MerchantsServeTest.java
@@ -2,13 +2,17 @@ package online.dh.couponsystem.service;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import online.dh.couponsystem.valueObject.CouponTemplate;
 import online.dh.couponsystem.valueObject.CreateMerchantsRequest;
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * <h2>商户服务测试类</h2>
@@ -26,9 +30,9 @@ public class MerchantsServeTest {
 //	@Transactional
 	public void testCreateMerchants() {
 		CreateMerchantsRequest merchantsRequest = new CreateMerchantsRequest();
-		merchantsRequest.setName("测试");
+		merchantsRequest.setName("测试2");
 		merchantsRequest.setLogoUrl("www.d-h.online");
-		merchantsRequest.setAddress("北京");
+		merchantsRequest.setAddress("云南");
 		merchantsRequest.setPhone("1234567890");
 		merchantsRequest.setBusinessLicenseUrl("www.d-h.online");
 
@@ -43,5 +47,20 @@ public class MerchantsServeTest {
 		System.out.println(JSON.toJSONString(merchantsServe.buildMerchantsInfoId(26)));
 		//{"errorCode":6,"errorMsg":"商户不存在"}
 		System.out.println(JSON.toJSONString(merchantsServe.buildMerchantsInfoId(25)));
+	}
+
+	@Test
+	public void testDropCouponTemplate(){
+		CouponTemplate couponTemplate = new CouponTemplate();
+		couponTemplate.setId(28);
+		couponTemplate.setTitle("测试2");
+		couponTemplate.setSummary("测试2");
+		couponTemplate.setDesc("测试2");
+		couponTemplate.setBackground(3);
+		couponTemplate.setHasToken(false);
+		couponTemplate.setStart(new Date());
+		couponTemplate.setEnd(DateUtils.addDays(new Date(), 10));
+
+		System.out.println(JSON.toJSONString(merchantsServe.dropCouponTemplate(couponTemplate)));
 	}
 }
